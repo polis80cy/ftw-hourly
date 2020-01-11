1801da9cf457d1e6affdf97d03019350a8cd2799
Polis Pilavas <polispilavas@Poliss-MacBook-Pro.local>
2020-01-11 23:45:06 +0800

    more changes

---
+++ src/components/CancellationPolicy/CancellationPolicy.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import css from './CancellationPolicy.css';
+
+const CancellationPolicy = props => {
+  const { rootClassName, className } = props;
+  const classes = classNames(rootClassName || css.root, className);
+
+  // prettier-ignore
+  return (
+    <div className={classes}>
+      <p className={css.lastUpdated}>Last updated: January 11, 2020</p>
+      
+      <p>
+        If a confirmed booking needs to be cancelled either by a photographer booking a studio
+        or the studio itself, then this Cancellation Policy applies. We encourage both
+        photographers and studios to communicate directly in the event of a cancellation as
+        soon as possible, and also immediately <a href="mailto:hello@jiffy.studio">contact Jiffystudio</a> to
+        initiate the cancellation process.
+      </p>
+      
+      <table>
+        <tr>
+          <th>Cancellation Date</th>
+          <th>Refund Amount</th>
+        </tr>
+        <tr>
+          <td>Booking request still pending</td>
+          <td>100% (incl. platform fee)</td>
+        </tr>
+        <tr>
+          <td>14 days or less ahead of booking start date</td>
+          <td>100% (minus platform fee)</td>
+        </tr>
+        <tr>
+          <td>7 days or less ahead of booking start date</td>
+          <td>50% (minus platform fee)</td>
+        </tr>
+        <tr>
+          <td>2 days or less ahead of booking start date</td>
+          <td>0% (minus platform fee)</td>
+        </tr>
+      </table>
+      
+      <p></p>
+      
+      <h3>Jiffystudio Initiated Cancellations</h3>
+      <p>
+        In some circumstances, Jiffystudio may decide to cancel a booking and initiate a
+        refund to both parties in accordance with this Cancellation Policy. Such circumstances
+        may include knowledge of potential illegal activity or harm, risk or safety concerns,
+        or any other scenario requiring such response.
+      </p>
+
+    </div>
+  );
+};
+
+CancellationPolicy.defaultProps = {
+  rootClassName: null,
+  className: null,
+};
+
+const { string } = PropTypes;
+
+CancellationPolicy.propTypes = {
+  rootClassName: string,
+  className: string,
+};
+
+export default CancellationPolicy;
---
+++ src/containers/CancellationPolicyPage/CancellationPolicyPage.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { FormattedMessage, injectIntl, intlShape } from '../../util/reactIntl';
+import { isScrollingDisabled } from '../../ducks/UI.duck';
+import { TopbarContainer } from '../../containers';
+import {
+  Page,
+  LayoutSideNavigation,
+  LayoutWrapperMain,
+  LayoutWrapperSideNav,
+  LayoutWrapperTopbar,
+  LayoutWrapperFooter,
+  Footer,
+  CancellationPolicy,
+} from '../../components';
+import config from '../../config';
+
+import css from './CancellationPolicyPage.css';
+
+const CancellationPolicyPageComponent = props => {
+  const { scrollingDisabled, intl } = props;
+
+  const tabs = [
+    {
+      text: intl.formatMessage({ id: 'TermsOfServicePage.privacyTabTitle' }),
+      selected: false,
+      linkProps: {
+        name: 'PrivacyPolicyPage',
+      },
+    },
+    {
+      text: intl.formatMessage({ id: 'TermsOfServicePage.tosTabTitle' }),
+      selected: false,
+      linkProps: {
+        name: 'TermsOfServicePage',
+      },
+    },
+    {
+      text: intl.formatMessage({ id: 'TermsOfServicePage.cancellationPolicyTabTitle' }),
+      selected: true,
+      linkProps: {
+        name: 'CancellationPolicyPage',
+      },
+    },
+  ];
+  const siteTitle = config.siteTitle;
+  const schemaTitle = intl.formatMessage({ id: 'CancellationPolicyPage.schemaTitle' }, { siteTitle });
+  const schema = {
+    '@context': 'http://schema.org',
+    '@type': 'WebPage',
+    name: schemaTitle,
+  };
+  return (
+    <Page title={schemaTitle} scrollingDisabled={scrollingDisabled} schema={schema}>
+      <LayoutSideNavigation>
+        <LayoutWrapperTopbar>
+          <TopbarContainer currentPage="CancellationPolicyPage" />
+        </LayoutWrapperTopbar>
+        <LayoutWrapperSideNav tabs={tabs} />
+        <LayoutWrapperMain>
+          <div className={css.content}>
+            <h1 className={css.heading}>
+              <FormattedMessage id="CancellationPolicyPage.heading" />
+            </h1>
+            <CancellationPolicy />
+          </div>
+        </LayoutWrapperMain>
+        <LayoutWrapperFooter>
+          <Footer />
+        </LayoutWrapperFooter>
+      </LayoutSideNavigation>
+    </Page>
+  );
+};
+
+const { bool } = PropTypes;
+
+CancellationPolicyPageComponent.propTypes = {
+  scrollingDisabled: bool.isRequired,
+
+  // from injectIntl
+  intl: intlShape.isRequired,
+};
+
+const mapStateToProps = state => {
+  return {
+    scrollingDisabled: isScrollingDisabled(state),
+  };
+};
+
+const CancellationPolicyPage = compose(
+  connect(mapStateToProps),
+  injectIntl
+)(CancellationPolicyPageComponent);
+
+export default CancellationPolicyPage;
