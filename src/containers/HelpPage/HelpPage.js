ad05007616c7e55999091e618ca4f75096e4d5d5
Polis Pilavas <polispilavas@Poliss-MacBook-Pro.local>
2020-02-13 19:03:30 +0800

    Format FAQs

--- src/containers/HelpPage/HelpPage.js
+++ src/containers/HelpPage/HelpPage.js
@@ -106,87 +106,98 @@ const HelpPage = () => {
 
               <h2>For hosts</h2>
 
-              <button class={css.collapsible}>Can I list my studio for free?</button>
-              <div class={css.content}>
-                Yes, listings are free. We only charge a commission when a booking has been accepted.
-              </div>
-
               <p>
-                <button class={css.collapsible}>Why do you need my banking info?</button>
-                <div class={css.content}>
-                  If we were to remove the banking info capture from the listing process, there would be no
-                  payments of any kind. Photographers wouldn't be able to pay at the time of booking
-                  (and secure your time and services in advance). You wouldn't be able to get
-                  (pre)paid for your studio services. And we wouldn't be able to maintain a free platform.
-                </div>
+                <Collapsible tabIndex={6} trigger="Can I list my studio for free?">
+                  <p>
+                    Yes, listings are free. We only charge a commission when a booking has been accepted.
+                  </p>
+                </Collapsible>
               </p>
 
               <p>
-                <button class={css.collapsible}>Why do you ask for my banking info upfront?</button>
-                <div class={css.content}>
-                  To discourage fraudulent listings, and power automatic payouts to you at the time of booking.
-                </div>
+                <Collapsible tabIndex={7} trigger="Why do you need my banking info?">
+                  <p>
+                    If we were to remove the banking info capture from the listing process, there would be no
+                    payments of any kind. Photographers wouldn't be able to pay at the time of booking
+                    (and secure your time and services in advance). You wouldn't be able to get
+                    (pre)paid for your studio services. And we wouldn't be able to maintain a free platform.
+                  </p>
+                </Collapsible>
               </p>
 
               <p>
-                <button class={css.collapsible}>What happens to my banking info once I share it?</button>
-                <div class={css.content}>
-                  We do <b><u>not</u></b> keep any banking info on our platform. The process of capturing
-                  your banking info comes from Stripe; The onboarding popup screens you get asking you
-                  for that info are presented and hosted by Stripe, completely hidden from us. Stripe is a
-                  certified payment processor, just like Square, Paypal, and Visa.
-                </div>
+                <Collapsible tabIndex={8} trigger="Why do you ask for my banking info upfront?">
+                  <p>
+                    To discourage fraudulent listings, and power automatic payouts to you at the time of booking.
+                  </p>
+                </Collapsible>
               </p>
 
               <p>
-                <button class={css.collapsible}>Why Stripe? Why not Paypal, Square or offline payments?</button>
-                <div class={css.content}>
-                  Stripe has built <a href="https://stripe.com/gb/connect">payment functionality</a> that caters
-                  specifically to the marketplace model we use. By using <i>Stripe Connect</i>, we power money
-                  acceptance, commissions, and payouts to third parties automatically. Paypal and Square
-                  do not offer this functionality. Offline doesn't scale (we are a small team) and would be
-                  costly to maintain.
-                </div>
+                <Collapsible tabIndex={9} trigger="What happens to my banking info once I share it?">
+                  <p>
+                    We do <b><u>not</u></b> keep any banking info on our platform. The process of capturing
+                    your banking info comes from Stripe; The onboarding popup screens you get asking you
+                    for that info are presented and hosted by Stripe, completely hidden from us. Your 
+                    banking info is kept with Stripe, an international certified payment processor, just
+                    like Square, Paypal, and Visa.
+                  </p>
+                </Collapsible>
               </p>
 
               <p>
-                <button class={css.collapsible}>How do I add payout information?</button>
-                <div class={css.content}>
-                  All accounts with a studio listing must add payout information in order to receive booking
-                    requests. We use Stripe Connect to power payments. Adding your payout information is easy
-                    and can be done in these steps:
-                    <ol type="1">
-                      <li>When logged into your Jiffystudio account, click on the top right account avatar
-                        and then “Account Settings” from the dropdown.</li>
-                      <li>On the left navigation, click on “Payout details”.</li>
-                      <li>Select your country and bank account information (required by Stripe).</li>
-                    </ol>
-                    If at any time you need help with this, please email us at <a href="mailto:hello@jiffy.studio">hello@jiffy.studio</a>.
-                </div>
+                <Collapsible tabIndex={10} trigger="Why Stripe? Why not Paypal, Square or offline payments?">
+                  <p>
+                    Stripe has built <a href="https://stripe.com/gb/connect">payment functionality</a> that caters
+                    specifically to the marketplace model we use. By using <i>Stripe Connect</i>, we power money
+                    acceptance, commissions, and payouts to third parties automatically. Paypal and Square
+                    do not offer this functionality. Offline doesn't scale (we are a small team) and would be
+                    costly to maintain.
+                  </p>
+                </Collapsible>
               </p>
 
               <p>
-                <button class={css.collapsible}>I accepted a booking request. When will I receive the money?</button>
-                <div class={css.content}>
-                  The amount that you made from the booking will appear in the bank account that you entered
-                  in your payout information within a few business days following the booking date (typically
-                  7-10 business days for the first booking, and much sooner for subsequent bookings). This
-                  process is handled by Stripe. Rest assured that once you accept a booking request, the person
-                  that booked your studio has already been charged for the booking and paid for it. If you
-                  have any questions about this at any time, please email us at <a href="mailto:hello@jiffy.studio">hello@jiffy.studio</a>.
-                </div>
+                <Collapsible tabIndex={11} trigger="How do I add payout information?">
+                  <p>
+                    All accounts with a studio listing must add payout information in order to receive booking
+                      requests. We use Stripe Connect to power payments. Adding your payout information is easy
+                      and can be done in these steps:
+                      <ol type="1">
+                        <li>When logged into your Jiffystudio account, click on the top right account avatar
+                          and then “Account Settings” from the dropdown.</li>
+                        <li>On the left navigation, click on “Payout details”.</li>
+                        <li>Select your country and bank account information (required by Stripe).</li>
+                      </ol>
+                      If at any time you need help with this, please email us at <a href="mailto:hello@jiffy.studio">hello@jiffy.studio</a>.
+                  </p>
+                </Collapsible>
+              </p>
+
+              <p>
+                <Collapsible tabIndex={11} trigger="I accepted a booking request. When will I receive the money?">
+                  <p>
+                    The amount that you made from the booking will appear in the bank account that you entered
+                    in your payout information within a few business days following the booking date (typically
+                    7-10 business days for the first booking, and much sooner for subsequent bookings). This
+                    process is handled by Stripe. Rest assured that once you accept a booking request, the person
+                    that booked your studio has already been charged for the booking and paid for it. If you
+                    have any questions about this at any time, please email us at <a href="mailto:hello@jiffy.studio">hello@jiffy.studio</a>.
+                  </p>
+                </Collapsible>
               </p>  
 
               <p>
-                <button class={css.collapsible}>How can I enforce a cancellation policy?</button>
-                <div class={css.content}>
-                  We have a standard <a href="https://www.jiffy.studio/cancellation-privacy-policy">
-                  Cancellation Policy</a>. We empower studios and photographers to communicate their
-                  availability directly. Changes in dates, booking modifications, cancellations, and
-                  rescheduling, all happen in accordance with our Cancellation Policy. In the event
-                  that this can not be agreed upon directly with the other party, you can email us
-                  at <a href="mailto:hello@jiffy.studio">hello@jiffy.studio</a>.
-                </div>
+                <Collapsible tabIndex={12} trigger="How can I enforce a cancellation policy?">
+                  <p>
+                    We have a standard <a href="https://www.jiffy.studio/cancellation-privacy-policy">
+                    Cancellation Policy</a>. We empower studios and photographers to communicate their
+                    availability directly. Changes in dates, booking modifications, cancellations, and
+                    rescheduling, all happen in accordance with our Cancellation Policy. In the event
+                    that this can not be agreed upon directly with the other party, you can email us
+                    at <a href="mailto:hello@jiffy.studio">hello@jiffy.studio</a>.
+                  </p>
+                </Collapsible>
               </p>       
             </div>
           </div>
